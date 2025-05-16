7faa194e3d719f280d45f42dceee1708fb0ba099
Hanh <htd2519@gmail.com>
2025-05-16 16:40:04 -0700

    dev phase 1 w fetch

--- src/webparts/rollingText/components/RollingText.tsx
+++ src/webparts/rollingText/components/RollingText.tsx
@@ -1,24 +1,39 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
+import { SPHttpClient } from '@microsoft/sp-http';
 import styles from './RollingText.module.scss';
-import type { IRollingTextProps } from './IRollingTextProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import type { IRollingTextProps } from "./IRollingTextProps";
 
-export default class RollingText extends React.Component<IRollingTextProps> {
-  public render(): React.ReactElement<IRollingTextProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+const RollingText: React.FC<IRollingTextProps> = (props) => {
+  const [items, setItems] = useState<string[]>([]);
 
-    return (
-      <section className={`${styles.rollingText} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
+  useEffect(() => {
+    const fetchListItems = async () => {
+      try {
+        const response = await props.context.spHttpClient.get(
+          `https://graph.microsoft.com/v1.0/sites/${props.context.pageContext.web.absoluteUrl}/lists/${props.listId}/items`,
+          SPHttpClient.configurations.v1
+        );
+        const data = await response.json();
+        const items = data.value.map((item: any) => item.fields[props.listContent]);
+        setItems(items);
+      } catch (error) {
+        console.error('Error fetching list items:', error);
+      }
+    }
+    fetchListItems();
+  }, [props.listId, props.listContent, props.context]);
+  // const { Speed, Direction, Delay, Loop } = props; 
 
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <div className={`${styles.rollingText}`}>
+      <div className={styles.container}>
+        {items.map((item, index) => (
+          <span key={index}>{item.trim()}</span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RollingText;
