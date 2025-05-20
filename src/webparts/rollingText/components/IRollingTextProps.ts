78c28a99180dbb451cf274998dca203378a5f610
Hanh <htd2519@gmail.com>
2025-05-20 15:33:45 -0700

    added props fixed spHttpClient

--- src/webparts/rollingText/components/IRollingTextProps.ts
+++ src/webparts/rollingText/components/IRollingTextProps.ts
@@ -1,16 +1,17 @@
 export interface IRollingTextProps {
   description: string;
   listContent: string;
-  listName: string;
+  listTitle: string;
   Text: string;
-  context: any;
-  lists: any;
-  listId: number;
-  items: string[];
-  item: string;
   Speed: number;
   Direction: string;
   Delay: number;
   Loop: boolean;
+  context: any; // Use a more specific type if available
+  lists: any;
+  listId: number;
+  items: string[];
+  item: string;
   // onPropertyChange: (propertyPath: string, newValue: any) => void;
 }
+
